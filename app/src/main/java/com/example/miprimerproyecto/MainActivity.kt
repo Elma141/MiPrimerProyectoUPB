858ef3bc6d4144de85583755d226cd448f80a149
Elma141 <elmines.rf@gmail.com>
2022-04-12 23:44:52 -0400

    Creamos rama develop2

--- app/src/main/java/com/example/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/example/miprimerproyecto/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.miprimerproyecto
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
